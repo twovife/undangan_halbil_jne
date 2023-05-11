0863458c4b633789df15498b021308715bf83f6f
twovife <azizihsan666@gmail.com>
2023-05-12 01:33:38 +0700

    build

--- pages/invitation/index.jsx
+++ pages/invitation/index.jsx
@@ -1,18 +1,9 @@
-import Brides from "@/components/Brides";
-import Cover from "@/components/Cover";
-import Date from "@/components/Date";
-import Head from "next/head";
 import { Poppins } from "next/font/google";
 import { useRouter } from "next/router";
-import Gallery from "@/components/Gallery";
-import Gift from "@/components/Gift";
-import { ToastContainer } from "react-toastify";
 
 // Toastify CSS
 import "react-toastify/dist/ReactToastify.css";
-import Wishes from "@/components/Wishes";
-import End from "@/components/End";
-import NavMenu from "@/components/NavMenu";
+
 import { useEffect } from "react";
 
 // AOS
